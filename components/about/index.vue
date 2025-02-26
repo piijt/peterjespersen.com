4c67c2a77e219ec2010ef0faa21ac5309beddb28
Peter Jespersen <piijt@hotmail.com>
2025-02-26 20:52:44 +0100

    text-updates etc

--- components/about/index.vue
+++ components/about/index.vue
@@ -2,11 +2,11 @@
     <div>
         <h1 class="font-bold space-x-2 py-4"><span class="primary cursor-pointer">Hi</span>, I'm Peter👋</h1>
         <p class="space-x-2 pb-2">🌍 Denmark, Odense</p>
-        <p class="space-x-2 pb-2">Software Developer, with expertise in system design & microservices. Thorough and
+        <p class="space-x-2 pb-4">Software Developer, with expertise in system design & microservices. Thorough and
             curious by nature who loves to build full-stack projects.</p>
         <p>Employment History</p>
         <div class="text-sm">
-            <div class="space-x-2 flex flex-col mt-2" v-for="entry in employment_history">
+            <div class="space-x-2 flex flex-col mt-2 pb-2" v-for="entry in employment_history">
                 <NuxtLink class="primary" :to="entry.website" target="_blank">
                     <span class="flex flex-row gap-x-2 align-center justify-content m-auto">
                         {{ entry.role }} - {{ entry.company }}
@@ -17,7 +17,7 @@
                         - {{ formatEmploymentHistory(entry) }}
                     </span>
                     <blockquote class="text-md text-gray-300 cursor-vertical-text pt-1">"{{ entry.brief }}"</blockquote>
-                    <div class=" pt-2 px-2 flex flex-row gap-2 flex-wrap">
+                    <div class=" pt-2 flex flex-row gap-2 flex-wrap">
                         <img v-for="t in entry.tech" :src="t" style="height: 25px;">
                     </div>
                 </div>
@@ -47,11 +47,12 @@ const employment_history = ref<employment_entry[]>([
         start_date: new Date('2023-12-01'),
         end_date: null,
         current: true,
-        brief: "My day to day work consists of maintaining and building new features in C++ / C# and Nuxt, where we @Kinematic make awesome software for the construction business",
+        brief: "At Kinematic, I develop and maintain software solutions in C++, C#, and Nuxt.js for the construction industry. My role involves building new features and optimizing existing functionalities to enhance user experience and efficiency.",
         tech: [
             "https://img.shields.io/badge/Kubernetes-282C34?style=for-the-badge&logo=kubernetes&logoColor=white",
             "https://img.shields.io/badge/Nuxt.js-282C34?style=for-the-badge&logo=nuxtdotjs&logoColor=white",
             "https://img.shields.io/badge/C%2B%2B-282C34?style=for-the-badge&logo=cplusplus&logoColor=white",
+            "https://img.shields.io/badge/TypeScript-282C34?logo=typescript&style=for-the-badge&link=",
             "https://img.shields.io/badge/Digital_Ocean-282C34?style=for-the-badge&logo=digitalocean&logoColor=white",
             "https://img.shields.io/badge/Node.js-282C34?logo=nodedotjs&style=for-the-badge&link=https://github.com/piijt?tab=repositories&q=&type=&language=javascript&sort=",
             "https://img.shields.io/badge/MySQL-282C34?logo=mysql&style=for-the-badge&logoColor=white&link=https://github.com/piijt?tab=repositories&q=mysql&type=&language=&sort=",
@@ -63,10 +64,12 @@ const employment_history = ref<employment_entry[]>([
         company: "Trade-Raid.com",
         start_date: new Date('2022-08-01'),
         end_date: new Date("2023-11-30"),
-        brief: "I was working fullstack, and when I was brought in on this project refactored the backend from legacy PhP codebase to node.js. \n \n Trade-raid.com enables gamers to trade digital values across games, to save money and not swipe their credit card twice.",
+        brief: "As a Fullstack Developer, I was responsible for refactoring the backend from a PHP codebase to Node.js, significantly improving performance and maintainability. Trade-Raid.com enables gamers to trade digital assets across different games, helping them save money by avoiding duplicate purchases.",
         tech: [
             "https://img.shields.io/badge/Nuxt.js-282C34?style=for-the-badge&logo=nuxtdotjs&logoColor=white",
             "https://img.shields.io/badge/AWS-282C34?style=for-the-badge&logo=amazonaws&logoColor=white",
+            "https://img.shields.io/badge/TypeScript-282C34?logo=typescript&style=for-the-badge&link=",
+            "https://img.shields.io/badge/PhP-282C34?logo=php&style=for-the-badge&link=",
             "https://img.shields.io/badge/Express-282C34?logo=express&style=for-the-badge&link=https://github.com/piijt?tab=repositories&q=&type=&language=javascript&sort=",
             "https://img.shields.io/badge/Node.js-282C34?logo=nodedotjs&style=for-the-badge&link=https://github.com/piijt?tab=repositories&q=&type=&language=javascript&sort=",
             "https://img.shields.io/badge/MongoDB-282C34?logo=mongodb&style=for-the-badge&link=https://github.com/piijt?tab=repositories&q=mongodb&type=&language=&sort=",
@@ -80,10 +83,11 @@ const employment_history = ref<employment_entry[]>([
         company: "Cavea.io",
         start_date: new Date('2020-03-01'),
         end_date: new Date("2022-09-30"),
-        brief: "Primary focus on backend and DevOps, working with GCP and node.js to build and deploy API's, microservices & and ETL pipelines.",
+        brief: "At Cavea.io, I focused on backend development and DevOps, working with GCP and Node.js to build and deploy APIs, microservices, and ETL pipelines. My contributions enhanced scalability and data processing efficiency.",
         tech: [
             "https://img.shields.io/badge/Kubernetes-282C34?style=for-the-badge&logo=kubernetes&logoColor=white",
             "https://img.shields.io/badge/Vue.js-282C34?logo=vuedotjs&style=for-the-badge",
+            "https://img.shields.io/badge/TypeScript-282C34?logo=typescript&style=for-the-badge&link=",
             "https://img.shields.io/badge/Google_Cloud-282C34?style=for-the-badge&logo=google-cloud&logoColor=white",
             "https://img.shields.io/badge/Node.js-282C34?logo=nodedotjs&style=for-the-badge&link=https://github.com/piijt?tab=repositories&q=&type=&language=javascript&sort=",
             "https://img.shields.io/badge/NestJS-282C34?style=for-the-badge&logo=nestjs&logoColor=white",
@@ -99,7 +103,7 @@ const employment_history = ref<employment_entry[]>([
         company: "Alfalaval",
         start_date: new Date('2018-09-01'),
         end_date: new Date("2019-08-01"),
-        brief: "Working with an e-business concept to imrpove and enable engineers that used alfalaval's products around the world to save cost and reduce CO2 emission when manufacturing product-lines in several industries.",
+        brief: "Worked on an e-business concept aimed at helping engineers optimize Alfa Laval's products, reducing manufacturing costs and CO2 emissions across multiple industries.",
         tech: []
     },
 ])
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,36 @@ export default defineNuxtConfig({
   css: [
     '~/assets/css/main.css',
   ],
-
+  app: {
+    head: {
+      link: [
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/favicon-96x96.png",
+          sizes:"96x96"
+        },
+        {
+          rel: "icon",
+          type: "image/svg+xml",
+          href: "/favicon.svg"
+        },
+        {
+          rel: "shortcut icon",
+          href: "/favicon.ico"
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "apple-touch-icon.png"
+        },
+        {
+          rel: "manifest",
+          href: "site.webmanifest"
+        }
+      ]
+    }
+  },
   vite: {
     plugins: [
       tailwindcss(),
